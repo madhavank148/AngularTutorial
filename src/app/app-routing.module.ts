7ce649061bf61300499195e744fb53077f09de03
Madhavan k <54049740+madhavank148@users.noreply.github.com>
2023-06-06 12:30:39 +0530

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SliderComponent } from './slider/slider.component';
+import { MovieShowsComponent } from './movie-shows/movie-shows.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"/home", component:SliderComponent}, 
+   {path:"movies", component:MovieShowsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
