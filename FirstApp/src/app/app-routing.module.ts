5753c3ef0a62e2de5a4c3dac13935959a1039cdc
Madhavan k <54049740+madhavank148@users.noreply.github.com>
2023-06-07 10:39:18 +0530

    aa

--- FirstApp/src/app/app-routing.module.ts
+++ FirstApp/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SliderComponent } from './slider/slider.component';
 import { MovieShowsComponent } from './movie-shows/movie-shows.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 const routes: Routes = [
   {path:"home", component:SliderComponent}, 
-   {path:"movies", component:MovieShowsComponent}];
+   {path:"movies", component:MovieShowsComponent},
+   {path:"movies/:movieName",component:MovieDetailsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FirstApp/src/app/movie-details/movie-details.component.ts
+++ FirstApp/src/app/movie-details/movie-details.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import  data  from '../../assets/Movie.json';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.css']
+  styleUrls: ['./movie-details.component.css'],
 })
 export class MovieDetailsComponent {
+  movieData: any[];
 
+  specificData: any;
+
+  movieTitle: any;
+
+  selectedMovieData: any;
+
+  constructor(private objRoute: ActivatedRoute) {
+    //dependency injection
+
+    this.movieData = data;
+  }
+
+  ngOnInit() {
+    //extract the parameter   //Rxjs - observables, observer, subscribe->notify, unsubsrcibe
+
+    console.log('ng on init fired');
+
+    this.objRoute.paramMap.subscribe((params) => {
+      this.movieTitle = params.get('movieName');
+
+      console.log(this.movieTitle);
+
+      this.getSelectedMovieData(this.movieTitle);
+    });
+
+    //display the respective movie data on the UI
+  }
+
+  getSelectedMovieData(mtitle: string) {
+    for (let d of this.movieData) {
+      if (d.title == mtitle) {
+        this.selectedMovieData = d;
+
+        break;
+      }
+    }
+  }
+
+  //http://localhost:4200/Movies/Drishyam
 }
